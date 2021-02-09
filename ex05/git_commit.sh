4ec594c986637e22736a21a1391a4324a2a0e05e
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-02-09 10:04:42 -0500

    4e commit

--- ex05/git_commit.sh
+++ ex05/git_commit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#Ce scrit renvoie l'ID des 5 commits les plus récent
+#Ce script renvoie l'ID des 5 commits les plus récent
 git log -5 --format=format:"%H"
---
+++ ex06/git_ignore.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Ce script fera la list de tout les fichiers ignorés par le dépot git dans le dépot local
+git status -s --ignored
